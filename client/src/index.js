007941e0c5f73875733a0122134fba9c0e4f3b4a
priyasharanam1 <p1910413219@gmail.com>
2024-10-13 11:57:44 +0530

    streams shared

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { SocketProvider } from "./context/SocketProvider";
@@ -9,11 +9,11 @@ import { SocketProvider } from "./context/SocketProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SocketProvider>
-      <BrowserRouter>
+    <BrowserRouter>
+      <SocketProvider>
         <App />
-      </BrowserRouter>
-    </SocketProvider>
+      </SocketProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
--- server/index.js
+++ server/index.js
@@ -1,29 +1,38 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
 
-const io = new Server(8000,{
+const io = new Server(8000, {
   cors: true,
 });
-// const app = express();
 
-// app.use(bodyParser.json());
-
-const emailToSocketMapping = new Map();
-const socketidToEmailMapping = new Map();
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
 
 io.on("connection", (socket) => {
-  console.log("New connection established:", socket.id);
+  console.log(`Socket Connected`, socket.id);
   socket.on("room:join", (data) => {
     const { email, room } = data;
-    emailToSocketMapping.set(email, socket.id);
-    socketidToEmailMapping.set(socket.id,email);
-    io.to(room).emit("user:joined", {email, id: socket.id});
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
     socket.join(room);
     io.to(socket.id).emit("room:join", data);
   });
-});
 
-// Start the HTTP server
-// app.listen(8000, () => console.log("HTTP server running at port 8000"));
-// io.listen(8001, () => console.log("Socket.io server running at port 8001"));
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
